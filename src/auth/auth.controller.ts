a1b330b029cf6bfde2fb32dcbc8430874be7276e
ltomassini <leandroctomassini@gmail.com>
2025-09-17 16:06:31 -0300

    feat: implement authentication microservice with user registration and login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { AuthService } from './auth.service';
+import { LoginUserDto, RegisterUserDto } from './dto';
+
+
+@Controller()
+export class AuthController {
+
+  constructor(
+    private readonly authService: AuthService
+  ) { }
+
+
+  @MessagePattern( 'auth.register.user' )
+  registerUser( @Payload() registerUserDto: RegisterUserDto ) {
+    return registerUserDto;
+  }
+
+  @MessagePattern( 'auth.login.user' )
+  loginUser( @Payload() loginUserDto: LoginUserDto ) {
+    return 'login user';
+  }
+
+  @MessagePattern( 'auth.verify.user' )
+  verifyToken() {
+    return 'verify user';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
+
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger( 'Auth microservice' );
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>( AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers
+    }
+  } );
+
+  await app.listen();
+
+  logger.log( `Auth microservice running on port ${ envs.port }` );
+
 }
+
 bootstrap();
